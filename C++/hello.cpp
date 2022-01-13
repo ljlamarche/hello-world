b85417360fc2f4f0a28123ef408513553ee1f0b5
Leslie Lamarche <leslie.lamarche@sri.com>
2022-01-13 11:13:35 -0800

    Copy programs for C, C++, and Fortran from old work.

---
+++ C++/hello.cpp
@@ -0,0 +1,8 @@
+// hello.cpp
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+    return 0;
+}
---
+++ Fortran/hello.for
@@ -0,0 +1,5 @@
+C    hello.for
+
+      program hello
+          print *, "Hello World!"
+      end program
